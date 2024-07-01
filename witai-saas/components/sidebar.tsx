67066b3dd2dc270b77604d0721b19dd0b16ada66
Dylan Dao <dylanpdao@gmail.com>
2024-07-01 19:16:32 +0000

    created dashboard layout

--- witai-saas/components/sidebar.tsx
+++ witai-saas/components/sidebar.tsx
@@ -1,9 +1,11 @@
 "use client";
+
 import Link from "next/link"
 import Image from "next/image"
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils"
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({weight: "600", subsets:["latin"]})
 
@@ -52,6 +54,8 @@ const routes = [
 ]
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return(
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2">
@@ -70,7 +74,7 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full justify start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded lg transition"
+              className={cn("text-sm group flex p-3 w-full justify start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
