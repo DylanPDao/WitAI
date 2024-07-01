181cbb2ac61c9959dadf7f4fa97236fd6acfbd5a
Dylan Dao <dylanpdao@gmail.com>
2024-07-01 17:11:00 +0000

    created sidebar menu items

---
+++ witai-saas/components/mobile-sidebar.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Menu } from "lucide-react"
+import { Button } from "./ui/button"
+
+const MobileSidebar = () => {
+  return (
+    <Button className="md:hidden" variant="ghost" size="icon">
+      <Menu />
+    </Button>
+  )
+}
+
+export default MobileSidebar
